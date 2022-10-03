4ebb73cda3722911b14b0c800a498531e34a6583
egorn <egor.nazarenko31@mail.ru>
2022-10-03 12:34:45 +0300

    Добавил функции удаления, сложения, умножения, а так же проверил их в тестах

--- Labe3/Lenght.cs
+++ Labe3/Lenght.cs
@@ -3,21 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Labe3
 {
-    public class Lenght
+    public enum MeasureType { m, km, kn ,M}
+    public class Length
     {
         private double value;
-        private string type;
-        public Lenght(double value, string type)
+        private MeasureType type;
+        public Length(double value, MeasureType type)
         {
             this.value = value;
             this.type = type;
         }
         public string Verbose()
         {
-            return String.Format("{0} {1}", this.value, this.type);
+            string typeVerbose = "";
+            switch (this.type)
+            {
+                case MeasureType.m:
+                    typeVerbose = "м/c.";
+                    break;
+                case MeasureType.km:
+                    typeVerbose = "км/c.";
+                    break;
+                case MeasureType.kn:
+                    typeVerbose = "уз.";
+                    break;
+                case MeasureType.M:
+                    typeVerbose = "М";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+        public static Length operator + (Length instance, double number)
+        {
+            var newValue = instance.value + number;
+            var lenght = new Length(newValue, MeasureType.m);
+            return lenght;
+        }
+        public static Length operator + (double number, Length instance)
+        {
+            return instance + number;
+        }
+        public static Length operator *(Length instance, double number)
+        {
+            
+            return new Length(instance.value * number, instance.type); ;
+        }
+        public static Length operator *(double number, Length instance)
+        {
+            return instance * number;
+        }
+        public static Length operator -(Length instance, double number)
+        {
+            return new Length(instance.value - number, instance.type); ;
+        }
+        public static Length operator -(double number, Length instance)
+        {
+            return instance - number;
         }
     }
 }
---
+++ Labe3Tests/LenghtTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labe3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labe3.Tests
+{
+    [TestClass()]
+    public class LengthTests
+    {
+ 
+        [TestMethod()]
+        public void AddNumberTest()
+        {
+            var length = new Length(1, MeasureType.m);
+            length = length + 2;
+            Assert.AreEqual("3 м/c.", length.Verbose());
+        }
+        [TestMethod()]
+        public void MulNumberTest()
+        {
+            var length = new Length(2, MeasureType.m);
+            length = length * 2;
+            Assert.AreEqual("4 м/c.", length.Verbose());
+        }
+        [TestMethod()]
+        public void SubNumberTest()
+        {
+            var length = new Length(3, MeasureType.m);
+            length = length - 2;
+            Assert.AreEqual("1 м/c.", length.Verbose());
+        }
+    }
+}
