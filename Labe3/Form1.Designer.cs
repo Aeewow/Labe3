3730183d9364f664482063feea9460162b313294
egorn <egor.nazarenko31@mail.ru>
2022-10-16 13:58:10 +0300

    Создана форма для сложени и вычитания

--- Labe3/Form1.Designer.cs
+++ Labe3/Form1.Designer.cs
@@ -28,19 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.cmbOperation = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(53, 29);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(150, 31);
+            this.txtFirst.TabIndex = 0;
+            this.txtFirst.TextChanged += new System.EventHandler(this.txtFirst_TextChanged);
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(53, 85);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(150, 31);
+            this.txtSecond.TabIndex = 1;
+            this.txtSecond.TextChanged += new System.EventHandler(this.txtSecond_TextChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(53, 149);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(190, 31);
+            this.txtResult.TabIndex = 2;
+            // 
+            // cmbOperation
+            // 
+            this.cmbOperation.FormattingEnabled = true;
+            this.cmbOperation.Items.AddRange(new object[] {
+            "+",
+            "-"});
+            this.cmbOperation.Location = new System.Drawing.Point(255, 83);
+            this.cmbOperation.Name = "cmbOperation";
+            this.cmbOperation.Size = new System.Drawing.Size(71, 33);
+            this.cmbOperation.TabIndex = 3;
+            this.cmbOperation.Text = "+";
+            this.cmbOperation.SelectedIndexChanged += new System.EventHandler(this.cmbOperation_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1516, 602);
+            this.Controls.Add(this.cmbOperation);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtFirst);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox txtFirst;
+        private TextBox txtSecond;
+        private TextBox txtResult;
+        private ComboBox cmbOperation;
     }
 }
--- Labe3/Form1.cs
+++ Labe3/Form1.cs
@@ -6,5 +6,57 @@ namespace Labe3
         {
             InitializeComponent();
         }
+        private void Calculate()
+        {
+            try
+            {
+                var firstValue = double.Parse(txtFirst.Text);
+                var secondValue = double.Parse(txtSecond.Text);
+
+                var firstLength = new Length(firstValue, MeasureType.m);
+                var secondLength = new Length(secondValue, MeasureType.m);
+
+               
+                Length sumLength;
+
+                
+                switch (cmbOperation.Text)
+                {
+                    case "+":
+                        
+                        sumLength = firstLength + secondLength;
+                        break;
+                    case "-":
+                        
+                        sumLength = firstLength - secondLength;
+                        break;
+                    default:
+                        sumLength = new Length(0, MeasureType.m);
+                        break;
+                }
+
+                txtResult.Text = sumLength.Verbose();
+
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+        }
+
+        private void txtFirst_TextChanged(object sender, EventArgs e)
+        {
+            Calculate();            
+        }
+
+        private void txtSecond_TextChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void cmbOperation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
     }
 }
